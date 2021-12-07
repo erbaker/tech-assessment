ad3a72cb69f2b3b948d3d0d1b26f19d516aca281
Robert Hoehle <Robert.Hoehle@cunamutual.com>
2021-12-07 13:19:53 -0600

    Initial project creation

---
+++ Tests/ControllerTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace ControllerTests
+{
+    public class ControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
