d61e0639953f7a8078bb52423e71d3619af6a8de
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-08-27 22:12:17 -0500

    Added Accessor unit tests for some base coverage of that class for this exercise

---
+++ CSharp/CSharp.Accessors.Tests/OrderAccesorTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CSharp.Accessors.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class OrderAccesorTests
+    {
+        [Test]
+        public void GetOrders_ExistingCustomer_ShouldReturnListOfOrders()
+        {
+            // Arrange
+            var orderAccessor = new OrderAccessor();
+
+            // Act
+            var orders = orderAccessor
+                .GetOrders(1);
+
+            // Assert
+            Assert.That(orders.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetOrders_NonExistingCustomer_ShouldReturnEmptyList()
+        {
+            // Arrange
+            var orderAccessor = new OrderAccessor();
+
+            // Act
+            var orders = orderAccessor
+                .GetOrders(100);
+
+            // Assert
+            Assert.That(orders, Is.Empty);
+        }
+    }
+}
